871a64d5c7a3d61cb933545c3e309006dd9c42dc
Bisonpasfuté <thibault.cothenet@orange.fr>
2026-05-31 02:43:13 +0200

    [Guardian] Add T16 2P & 4P bonus

--- ui/core/proto_utils/action_id.ts
+++ ui/core/proto_utils/action_id.ts
@@ -966,6 +966,11 @@ export class ActionId {
 					name += ' (DoT)';
 				}
 				break;
+			case 'Frenzied Regeneration':
+				if (tag == 1) {
+					name += ' - Druid T16 Guardian 2P Bonus';
+				}
+				break;
 			default:
 				if (tag) {
 					name += ' (??)';
